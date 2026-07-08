1d7f01b9e45927e5611131ee2af94583fae9ade1
wuli-wa <1161353741@qq.com>
2026-07-08 11:38:27 +0800

    Site updated: 2026-07-08 11:38:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/04/10/hello-world/","2026/07/07/芝士甚么-WIFI密码/"];function toRandomPost(){
+var posts=["2026/07/07/芝士甚么-WIFI密码/","2026/04/10/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
